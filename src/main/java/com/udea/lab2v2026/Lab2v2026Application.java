3281b4d5ae75b8de7b7bba74f30e5de4e4aad8c4
DB <diego.botia@udea.edu.co>
2026-09-25 09:07:40 -0500

    first commit

---
+++ src/main/java/com/udea/lab2v2026/Lab2v2026Application.java
@@ -0,0 +1,13 @@
+package com.udea.lab2v2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2v2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2v2026Application.class, args);
+	}
+
+}
